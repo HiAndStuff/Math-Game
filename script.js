b110f83fcec78a9114a7f0ac5ce8324e671eb8fd
HiAndStuff <dacrazyyy17@gmail.com>
2026-05-07 10:49:18 +1000

    Update script.js
    A little test.

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const youCanThinkPad = document.getElementById('youCanThinkPad');
-const ctx = canvas.getContext('2d');
+const think = youCanThinkPad.getContext('2d');
+var questionNum = 1
+think.font = "30px Arial";
+think.fillText("Hello World", 400, 300);
