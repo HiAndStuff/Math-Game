2d9af0e1d2a7741d3f399946680944e383e98988
HiAndStuff <dacrazyyy17@gmail.com>
2026-05-07 13:03:15 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ const youCanThinkPad = document.getElementById('youCanThinkPad');
 const think = youCanThinkPad.getContext('2d');
 var questionNum = 1
 think.font = "100px Arial";
-think.fillText("WIP", screen.availWidth / 2, screen.availHeight / 2);
+think.fillText("WIP", (screen.availWidth / 2) - 50, screen.availHeight / 2);
